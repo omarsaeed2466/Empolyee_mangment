303f465e2a2ccb80aa492691f1c345153445e986
omarsaeed2466 <47633542+omarsaeed2466@users.noreply.github.com>
2022-08-16 02:18:38 +0200

    intial commit

---
+++ src/main/java/com/example/empolyee_mangment/EmpolyeeMangmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.empolyee_mangment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpolyeeMangmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpolyeeMangmentApplication.class, args);
+    }
+
+}
